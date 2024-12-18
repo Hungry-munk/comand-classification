4781113d6baff33e58791369cdd2e91d8bb5f51a
Hungry_munk <abbaszadeharsam@gmail.com>
2024-12-18 18:06:12 +1100

    datapipline workes and can load both one hots and spectrograms seemingly fine

--- configs.py
+++ configs.py
@@ -1,17 +1,25 @@
 class Configs:
     """ 
     A class created purely to store some global variable values needed across all files .
-    The class is to be used purely for image preprocessing, model training (variable storage)
-    and batch gene
+    The class is to be used purely for sound preprocessing, model training (variable storage)
+    and batch gen
     """
 
     def __init__(self):
         self.base_dir = './data/'
+        # self.spectrogram_configs = {
+        #     'nfft' : 256,
+        #     'window' : 256, #should match nfft
+        #     'stride' : 128 #should be half of nfft
+        # }
         self.spectrogram_configs = {
-            'nfft' : 256,
-            'window' : 256, #should match nfft
-            'stride' : 128 #should be half of nfft
+            'frame_length' : 512, 
+            'frame_step': 256
         }
+        self.train_batch_size = 32
+        self.val_batch_size = 16
+        self.test_batch_size = ...
+        self.target_rate = 16000 #16khz mono audio
         self.label_encodings = {
             "bed": 0,
             "bird": 1,
--- data_processing.py
+++ data_processing.py
@@ -5,6 +5,9 @@ import tensorflow_io as tfio
 import numpy as np
 from configs import Configs as C
 import math
+# global variables
+c = C()
+num_classes = c.num_classes
 
 # converting stereo audio into mono for 1d convelution and training
 def convert_to_mono(wav):
@@ -29,52 +32,69 @@ def prepare_wav(file_path, target_rate):
     # load audio file in
     audio_binary = tf.io.read_file(file_path)
     wav, sample_rate = tf.audio.decode_wav(audio_binary)
-
+    # come back later to .audio
+    # wav = wav.audio
     # Resample to 16kHz if necessary
     if sample_rate != target_rate:
         wav = resample_wav(wav, sample_rate, target_rate)
 
+    if wav.shape[0] < target_rate:
+        target_length = target_rate * 1
+        end_padding = target_length - tf.shape(wav)[0]
+        paddings = [[0, end_padding], [0,0]]
+        wav = tf.pad(wav, paddings, mode='constant', constant_values=0)
+
     # Convert to mono by taking the first channel if stereo
     if wav.shape[-1] > 1 :
         wav = convert_to_mono(wav)
+    # pad the wav to 1 sec
+
 
     return wav
-
-def wav_to_spectrogram(wav_tensor, nfft, window, stride):
-    return tfio.audio.spectrogram(
-        wav_tensor, 
-        nfft,
-        window,
-        stride
+def wav_to_spectrogram(wav_tensor, frame_length=255, frame_step=128):
+    if len(wav_tensor.shape) > 1:
+        wav_tensor = tf.squeeze(wav_tensor)
+    # Compute the Short-time Fourier Transform (STFT)
+    stft = tf.signal.stft(
+        signals=wav_tensor, 
+        frame_length=frame_length, 
+        frame_step=frame_step
     )
+    
+    # Compute the magnitude of the STFT
+    spectrogram = tf.abs(stft)
+    
+    return spectrogram
 
 # A function that is going to take in a set of file paths and retrun the appriopriate X and Y object for that data
-def batch_generator(X_file_paths, batch_size, target_rate, nfft, window, stride):
+def batch_generator(X_file_paths, batch_size, target_rate, frame_length, frame_step):
     x_data = []
     y_data = []
 
+    # trackers
+    data_example_count = len(X_file_paths)
     batch_length_count = 0
     for x_path in X_file_paths:
         # iterate thorugh files and create a data set for using those file paths
         
         # get label for path by splitting and indexing into secound item (first is base dir)
-        label = x_path.split(os.path.sep)[1]
+        label = x_path.split('/')[2]
         # create one hot encoding for softmax
-        one_hot = np.ones(C.num_classes)
-        one_hot_index = C.label_encodings[label]
+        one_hot = np.zeros(num_classes)
+        one_hot_index = c.label_encodings[label]
         one_hot[one_hot_index] = 1
-
-        # prepare spectrogram
+        # get wav audio
         wav = prepare_wav(x_path, target_rate)
-        spectrogram = wav_to_spectrogram(wav, nfft, window, stride)
+        # prepare spectrogram
+        spectrogram = wav_to_spectrogram(wav, frame_length, frame_step)
         # logormithic normalziation of spectrogram
         spectrogram = tf.cast(spectrogram, tf.float32)
         spectrogram = tf.math.log1p(spectrogram) #log1p is a better version of log + epsillon
 
         # add data to dataset
-        batch_length_count += 1
         x_data.append(spectrogram)
         y_data.append(one_hot)
+        batch_length_count += 1
 
         if batch_length_count >= batch_size:
             # pre pare and yield batch
@@ -86,26 +106,33 @@ def batch_generator(X_file_paths, batch_size, target_rate, nfft, window, stride)
             y_data = y_data[batch_size:]
             # reduce counter
             batch_length_count -= batch_size
+            data_example_count -= batch_size
+        # When their is not data for a full batch 
+        elif data_example_count < batch_size :
+            return np.array(x_data), np.array(y_data)
 
-def calculate_spectrogram_dimensions(target_rate, nfft, window, stride):
-    height = (nfft // 2) + 1
 
-    # Calculate width (time frames)
-    width = math.ceil((target_rate - window) / stride) + 1
-
-    return height, width 
+def calculate_spectrogram_dimensions(audio_time, target_rate, frame_length, frame_step):
+    audio_length = audio_time * target_rate
+    # Calculate number of frames
+    num_frames = 1 + (audio_length - frame_length) // frame_step
+    
+    # Number of frequency bins is half the frame length + 1
+    num_frequency_bins = (frame_length // 2) + 1
+    
+    return (num_frames, num_frequency_bins)
 
 #A function to create tensorflow data set for better GPU acceleration during training, validation and testing
-def create_dataset(X_file_paths, batch_size, target_rate, nfft, window, stride  ):
-    num_classes = C.num_classes
+def create_dataset(X_file_paths, batch_size, target_rate, frame_length, frame_step  ):
     # calcualte  spectrogram height and width
-    height, width = calculate_spectrogram_dimensions(target_rate, nfft, window, stride)
+    audio_time = 1
+    height, width = calculate_spectrogram_dimensions(audio_time, target_rate, frame_length, frame_step)
     
     dataset = tf.data.Dataset.from_generator(
-        lambda: batch_generator(X_file_paths, batch_size, target_rate, nfft, window, stride ,),
+        lambda: batch_generator(X_file_paths, batch_size, target_rate, frame_length, frame_step),
         output_signature = (
-            tf.TensorSpec(shape=(batch_size, height, width, 1 ), dtype= tf.float32), #define the shape of X's and Y's
-            tf.TensorSpec(shape=(batch_size, num_classes), dtype=tf.int32)
+            tf.TensorSpec(shape=(batch_size, height, width), dtype= tf.float32), #define the shape of X's and Y's
+            tf.TensorSpec(shape=(None, num_classes), dtype=tf.int32)
         )
     )
 
