407a2a4e313b1a458c7ef84a0963a35106feb755
Hungry_munk <abbaszadeharsam@gmail.com>
2024-12-19 10:38:25 +1100

    training waas doneand it was seemingly successful

--- model.py
+++ model.py
@@ -18,12 +18,12 @@ def build_CNN(input_shape, num_classes, main_activation = 'relu'):
     input_tensor = Input(input_shape)
     x = input_tensor
 
-    x = conv_bn_pool_block(x , 32, (3, 3), 'same', '1', main_activation, True, False)
+    x = conv_bn_pool_block(x , 32, (3, 3), 'same', '1', main_activation, True, True, (2,2))
     x = conv_bn_pool_block(x, 64, (3, 3), 'same', '2', main_activation, True, True, (2, 2))
     
     x = Flatten()(x)
 
-    x = Dense(128, activation = main_activation)(x)
+    x = Dense(32, activation = main_activation)(x)
     x = Dropout(0.3)(x)
 
     output_tensor = Dense(num_classes, activation = 'softmax')(x)
